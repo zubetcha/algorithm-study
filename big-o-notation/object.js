b3a7bb5b46303a07f7fbe5b96d2321c49011804c
zubetcha <zuhye5@gmail.com>
2022-10-23 18:59:40 +0900

    :sparkles: object with big-o

---
+++ big-o-notation/object.js
@@ -0,0 +1,10 @@
+const zubetcha = {
+  name: 'juhye',
+  age: 29,
+  liveInSeoul: true,
+};
+
+// 객체에 접근, 추가, 제거하는 작업은 O(1) -> 빠르다.
+// 탐색은 O(n)
+// Object.keys(), Object.values(), Object.entries -> key의 개수에 따라 실행 속도가 달라짐 -> O(n)
+// Object.hasOwnProperty()는 객체의 접근과 비슷 -> O(1)
