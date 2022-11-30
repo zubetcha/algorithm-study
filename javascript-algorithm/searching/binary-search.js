89be0c0652570087125003e59779d11b8941fc0e
zubetcha <zuhye5@gmail.com>
2022-11-30 09:11:55 +0900

    binary search

--- javascript-algorithm/searching/binary-search.js
+++ javascript-algorithm/searching/binary-search.js
@@ -1,3 +1,4 @@
+// 내 풀이
 function binarySearch(array, value) {
   let min = 0;
   let max = array.length - 1;
@@ -17,4 +18,18 @@ function binarySearch(array, value) {
   }
 
   return -1;
+}
+
+// 강의
+function binarySearch2(arr, elem) {
+  let start = 0;
+  let end = arr.length -1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== elem && start <= end) {
+    elem < arr[middle] ? end = middle - 1 : start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+
+  return arr[middle] === elem ?  middle : -1;
 }
